60385741bcb3d45a1ab219d025dcd221f8362db4
HiDefStatic <timothy.s.deel@gmail.com>
2017-05-18 15:23:11 -0700

    initial commit for finalized portfolio, ready to launch

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'main_page/index'
+
+  root 'main_page#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
